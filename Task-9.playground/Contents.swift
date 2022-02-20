18c5219cfbea73584aaf9fd992068d0463ca5645
Kamran <iskama98@mail.ru>
2022-02-20 18:33:28 +0300

    refactor: rename task

--- Task-9.playground/Contents.swift
+++ Task-9.playground/Contents.swift
@@ -1,127 +1,99 @@
 //Task-9
 /*
- 1. Создайте пару функций с короткими именами, которые возвращают строку с классным символом или символами. Например heart() возвращает сердце и т.п. Вызовите все эти функции внутри принта для вывода строки этих символов путем конкатенации.
+ 1. Создать строку произвольного текста, минимум 200 символов. Используя цикл и оператор свитч посчитать количество гласных, согласных, цифр и символов.
 
- 2. Опять шахматные клетки. Реализовать функцию, которая принимает букву и символ и возвращает строку “белая” или “черная”. Строку потом распечатайте в консоль
+ 2. Создайте свитч который принимает возраст человека и выводит описание жизненного этапа
 
- 3. Создайте функцию, которая принимает массив, а возвращает массив в обратном порядке. Можете создать еще одну, которая принимает последовательность и возвращает массив в обратном порядке. Чтобы не дублировать код, сделайте так, чтобы функция с последовательностью вызывала первую.
+ 3. У вас есть имя отчество и фамилия студента (русские буквы). Имя начинается с А или О, то обращайтесь к студенту по имени, если же нет, то если у него отчество начинается на В или Д, то обращайтесь к нему по имени и отчеству, если же опять нет, то в случае если фамилия начинается с Е или З, то обращайтесь к нему только по фамилии. В противном случае обращайтесь к нему по полному имени.
 
- 4. Разберитесь с inout самостоятельно и выполните задание номер 3 так, чтобы функция не возвращала перевернутый массив, но меняла элементы в существующем. Что будет если убрать inout?
-
- 5. Создайте функцию, которая принимает строку, убирает из нее все знаки препинания, делает все гласные большими буквами, согласные маленькими, а цифры меняет на соответствующие слова (9 -> nine и тд)
+ 4. Представьте что вы играете в морской бои и у вас осталось некоторое количество кораблей на поле 10Х10 (можно буквы и цифры, а можно только цифры). Вы должны создать свитч, который примет тюпл с координатой и выдаст один из вариантов: мимо, ранил, убил.
+ 44:52
  */
 
 print("-------------Task-1-------------")
 
-func heart() -> String{
-    return "<3"
+let str: String = "Lore2 ipsum dolor sit amet, (on2ectetur adi2iscing elit, sed 4o eius1od )empor incdidu0t u=t labore et dol9re magna a9iqua. Ut enim ad minim ve6iam, quis no<trud e3ercitation ull8mco lab>ris nzsi ut ."
+
+var vowels = 0
+var constants = 0
+var numbers = 0
+var symbols = 0
+
+
+for char in str.lowercased() {
+    switch char {
+    case "a", "e", "i", "u", "o" :
+        vowels += 1
+    case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "y", "z" :
+        constants += 1
+    case ",", ".", " ", ")", "(", ">", "<", "=" :
+        symbols += 1
+    case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+        numbers += 1
+    default: break
+    }
 }
 
-func smile() -> String {
-    return "8)"
-}
-
-print(heart())
-print(smile())
-print("Heart: " + heart() + " / " + "Smile: " + smile())
+print("At our string: \(str),\n WE HAVE: \(vowels) -> vowels, \(constants) -> constants, \(symbols) -> symbols, \(numbers) -> numbers")
 
 print("-------------Task-2-------------")
-/*
- 
- 2. Опять шахматные клетки. Реализовать функцию, которая принимает букву и символ и возвращает строку “белая” или “черная”. Строку потом распечатайте в консоль
- */
 
-func chessCage(hor: String, ver: String) -> String {
-    let letter = ["a" , "b", "c", "d", "e", "f", "g", "h"]
-    let num = ["1" , "2", "3", "4", "5", "6", "7", "8"]
-    
-    var chess = [String:String]()
-    var color = ""
-    for i in 0..<letter.count {
-        for j in 0..<num.count {
-            chess[(letter[i]) + (num[j])] = (i % 2 == j % 2) ? "Black" : "White"
-        }
-    }
-    
-    for (i, j) in chess {
-        if i == (hor + ver) {
-            color = j
-        }
-    }
-    return color
+let age = 120
+
+switch age {
+case(let age) where age <= 0 :
+    print("Ur not born")
+case 1..<3 :
+    print("Ur child")
+case 3..<7:
+    print("Little bit older, than child")
+case 7..<16:
+    print("Ur youth")
+case 16..<18:
+    print("Little bit older, than youth")
+case 18..<35:
+    print("Ur young")
+case 35..<50:
+    print("Little bit older, than young")
+case 50..<80:
+    print("Ur old man")
+case 80..<100:
+    print("Older, than old")
+case 100..<120:
+    print("Ur long-liver")
+case (let age) where age >= 120:
+    print("I dont think ur >= 120 yrs old")
+
+default: break
 }
-
-let position = chessCage(hor: "d", ver: "6")
-print("Position is \(position)")
-
-
-func chess(Vertical v: Int, Horizontal h: String) -> String {
-    let hNumber = (h == "a" || h == "c" || h == "e" || h == "g") ? 1 : 0
-    let color = ((v + hNumber) % 2 == 0 ) ? "Black" : "White"
-    return "Cell: " + "(" + String(v) + h + ")" + " " + "-> " + color + " Color"
-}
-
-print(chess(Vertical: 6, Horizontal: "d"))
 
 print("-------------Task-3-------------")
 
-func reverseArr(input: [Int]) -> [Int] {
-    var temp = [Int]()
-    
-    for num in input {
-        temp.insert(num, at: 0)
-    }
-    return temp
+let student = (name: "Зубайра", surname: "Тухугов", patronymic: "Алиханович")
+
+switch student {
+case let(name, _, _) where name.hasPrefix("А") || name.hasPrefix("О"):
+    print("Hello,", student.name)
+case let(_, _, patronymic) where patronymic.hasPrefix("В") || patronymic.hasPrefix("Д"):
+    print("Hello,", student.name, student.patronymic)
+case let(_, surname, _) where surname.hasPrefix("Е") || surname.hasPrefix("З"):
+    print("Hello,", student.surname)
+default: print("Hello,", student.surname, student.name, student.patronymic)
 }
-
-var array = [5, 500, 1, 10, 100, 1000, 5000]
-
-print(reverseArr(input: array))
-
-func makeArr(input: Int...) -> [Int] {
-    let inp1 = input
-    
-    return reverseArr(input: inp1)
-}
-
-print(makeArr(input: 10, 11, 12, 13, 14, 15, 16))
 
 print("-------------Task-4-------------")
 
-func funcInOut (input: inout [Int]) {
-    for num in input {
-        input.insert(num, at: 0)
-        input.removeLast()
-    }
+let coordinate = ("E", 3)
+
+switch coordinate {
+case("A", 1):
+    print("Miss")
+case("E",3):
+    print("Ur ship is Dead")
+case("F", 4):
+    print("Ur ship is Wounded")
+default: break
 }
 
-funcInOut(input: &array)
-print(funcInOut(input: &array))
 
-print("-------------Task-5-------------")
 
-var someStr = "Lore2 ipsum dolor sit amet, (on2ectetur adi2iscing elit, sed 4o eius1od )empor incdidu0t u=t labore et dol9re magna a9iqua. Ut enim ad minim ve6iam, quis no<trud e3ercitation ull8mco lab>ris nzsi ut ."
-
-func getStr(str: String) -> String {
-    let nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    var otherStr = ""
-    
-    for i in str.lowercased() {
-        
-        switch i {
-        case "a", "e", "u", "i", "o", "y" :
-            otherStr += i.uppercased()
-        case "b"..."z":
-            otherStr += i.lowercased()
-        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-            otherStr += nums[Int(String(i))!]
-        case " ":
-            otherStr += " "
-        default:
-            otherStr += ""
-        }
-    }
-    return otherStr
-}
-
-print(getStr(str: someStr))
