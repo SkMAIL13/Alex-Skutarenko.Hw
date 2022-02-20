d304fa6dc3e9c3a58d3f62b51039c7b6d2738fdf
Kamran <iskama98@mail.ru>
2022-02-20 18:33:03 +0300

    refactor: rename task

--- Task-8.playground/Contents.swift
+++ Task-8.playground/Contents.swift
@@ -1,99 +1,94 @@
 //Task-8
 /*
- 1. Создать строку произвольного текста, минимум 200 символов. Используя цикл и оператор свитч посчитать количество гласных, согласных, цифр и символов.
+ 1. Создайте дикшинари как журнал студентов, где имя и фамилия студента это ключ, а оценка за контрольную значение. Некоторым студентам повысьте оценки - они пересдали. Потом добавьте парочку студентов, так как их только что перевели к вам в группу. А потом несколько удалите, так как они от вас ушли :(
 
- 2. Создайте свитч который принимает возраст человека и выводит описание жизненного этапа
+ После всех этих перетрубаций посчитайте общий бал группы и средний бал
 
- 3. У вас есть имя отчество и фамилия студента (русские буквы). Имя начинается с А или О, то обращайтесь к студенту по имени, если же нет, то если у него отчество начинается на В или Д, то обращайтесь к нему по имени и отчеству, если же опять нет, то в случае если фамилия начинается с Е или З, то обращайтесь к нему только по фамилии. В противном случае обращайтесь к нему по полному имени.
+ 2. Создать дикшинари дни в месяцах, где месяц это ключ, а количество дней - значение.
+ В цикле выведите ключ-значение попарно, причем один раз выведите через тюплы, а другой раз пройдитесь по массиву ключей и для каждого из них доставайте значения.
 
- 4. Представьте что вы играете в морской бои и у вас осталось некоторое количество кораблей на поле 10Х10 (можно буквы и цифры, а можно только цифры). Вы должны создать свитч, который примет тюпл с координатой и выдаст один из вариантов: мимо, ранил, убил.
- 44:52
+ 3. Создать дикшинари , в которой ключ это адрес шахматной клетки (пример: a5, b3, g8), а значение это Bool. Если у клетки белый цвет, то значение true, а если черный - false. Выведите дикшинари в печать и убедитесь что все правильно.
+
+ Рекомендация: постарайтесь все сделать используя вложенный цикл (объяснение в уроке).
  */
+
+import Darwin
 
 print("-------------Task-1-------------")
 
-let str: String = "Lore2 ipsum dolor sit amet, (on2ectetur adi2iscing elit, sed 4o eius1od )empor incdidu0t u=t labore et dol9re magna a9iqua. Ut enim ad minim ve6iam, quis no<trud e3ercitation ull8mco lab>ris nzsi ut ."
+var classRegister = [
+    "Ivanov Ivan": 5,
+    "Semenov Semen": 3,
+    "Fedorov Fedor": 2
+]
 
-var vowels = 0
-var constants = 0
-var numbers = 0
-var symbols = 0
+print(classRegister)
 
+classRegister["Fedorov Fedor"] = 5
+classRegister.updateValue(4, forKey: "Semenov Semen")
+print("Marks after work on mistakes: \(classRegister)")
 
-for char in str.lowercased() {
-    switch char {
-    case "a", "e", "i", "u", "o" :
-        vowels += 1
-    case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "y", "z" :
-        constants += 1
-    case ",", ".", " ", ")", "(", ">", "<", "=" :
-        symbols += 1
-    case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-        numbers += 1
-    default: break
-    }
+classRegister["Dmitriy Dmitriev"] = 3
+classRegister["Potapov Potap"] = 3
+print("Our class with new classmates:", classRegister)
+
+classRegister["Ivanov Ivan"] = nil
+classRegister.removeValue(forKey: "Fedorov Fedor")
+print("Class without some students: \(classRegister)")
+
+print("-----General Summ of marks-----")
+var generalSum = 0
+for value in classRegister.values {
+    generalSum += value
+    
+}
+print("General sum of Students is:", generalSum)
+
+print("-----Average Summ of marks-----")
+let averageSum = Double(generalSum) / Double(classRegister.count)
+print("Average summ of Students is:", averageSum)
+
+/*
+ 2. Создать дикшинари дни в месяцах, где месяц это ключ, а количество дней - значение.
+ В цикле выведите ключ-значение попарно, причем один раз выведите через тюплы, а другой раз пройдитесь по массиву ключей и для каждого из них доставайте значения.
+
+ */
+print("-------------Task-2-------------")
+var monDict = ["Jan": 31, "Feb": 28, "Mar": 31, "Apr": 30, "May": 31, "Jun": 30, "Jul": 31, "Aug": 31, "Sep": 30, "Oct": 31, "Nov": 30, "Dec": 31]
+
+print("--------Method with Tuples------")
+
+for (key, value) in monDict {
+    print(key, "=", value)
 }
 
-print("At our string: \(str),\n WE HAVE: \(vowels) -> vowels, \(constants) -> constants, \(symbols) -> symbols, \(numbers) -> numbers")
-
-print("-------------Task-2-------------")
-
-let age = 120
-
-switch age {
-case(let age) where age <= 0 :
-    print("Ur not born")
-case 1..<3 :
-    print("Ur child")
-case 3..<7:
-    print("Little bit older, than child")
-case 7..<16:
-    print("Ur youth")
-case 16..<18:
-    print("Little bit older, than youth")
-case 18..<35:
-    print("Ur young")
-case 35..<50:
-    print("Little bit older, than young")
-case 50..<80:
-    print("Ur old man")
-case 80..<100:
-    print("Older, than old")
-case 100..<120:
-    print("Ur long-liver")
-case (let age) where age >= 120:
-    print("I dont think ur >= 120 yrs old")
-
-default: break
+print("--------Method with Arrays------")
+for key in monDict.keys {
+    print("\(key) -> \(monDict[key]!)")
 }
 
 print("-------------Task-3-------------")
+var chess: [String:Bool] = [:]
 
-let student = (name: "Зубайра", surname: "Тухугов", patronymic: "Алиханович")
+let numbersChess = [1, 2, 3, 4, 5, 6, 7, 8]
+let letterChess = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
-switch student {
-case let(name, _, _) where name.hasPrefix("А") || name.hasPrefix("О"):
-    print("Hello,", student.name)
-case let(_, _, patronymic) where patronymic.hasPrefix("В") || patronymic.hasPrefix("Д"):
-    print("Hello,", student.name, student.patronymic)
-case let(_, surname, _) where surname.hasPrefix("Е") || surname.hasPrefix("З"):
-    print("Hello,", student.surname)
-default: print("Hello,", student.surname, student.name, student.patronymic)
+for i in 0..<numbersChess.count {
+    for j in 0..<letterChess.count {
+        chess[letterChess[i] + String(numbersChess[j])] = (i % 2 == j % 2) ? false : true
+    }
 }
 
-print("-------------Task-4-------------")
+var typePos = "E6"
+print("Chess Cage: \(typePos) \(chess[typePos] == true ? "white" : "black") ")
 
-let coordinate = ("E", 3)
+print("-------------Second variant-------------")
 
-switch coordinate {
-case("A", 1):
-    print("Miss")
-case("E",3):
-    print("Ur ship is Dead")
-case("F", 4):
-    print("Ur ship is Wounded")
-default: break
+var chess1 : [String:Bool] = [:]
+
+for (index, letter) in letterChess.enumerated() {
+    for number in numbersChess {
+        chess1.updateValue((index + number) % 2 == 0, forKey: (letter + String(number)))
+    }
 }
-
-
-
+print(chess1)
